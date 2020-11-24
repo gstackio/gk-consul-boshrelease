a3bf022d4c493bbcb18fa19c9f50b23f80fe5b8e
Benjamin Gandon <benjamin@gstack.io>
2020-11-24 19:53:29 +0100

    Update team name

--- ci/consul-bump/repipe.sh
+++ ci/consul-bump/repipe.sh
@@ -8,7 +8,7 @@ pushd "${SCRIPT_DIR}" > /dev/null
 
 (
     set -x
-    fly --target="gk" \
+    fly --target="gk-plat-devs" \
         set-pipeline --pipeline="consul-bump" \
         --config="consul-bump-pipeline.yml" \
         --load-vars-from="../config.yml"
